ed36c81358d822d2568ae777264f7a9155c34ac3
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-09-03 14:46:14 -0400

    react review

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Heros from "./src/Heros";
+
+function App() {
+  return (
+    <div>
+      <button> Start </button>
+      <h1> Hello World </h1>
+      <Heros />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Heros.js
@@ -0,0 +1,12 @@
+function Heros() {
+  return (
+    <div>
+      <img
+        src="https://www.google.com/search?q=wolverine&rlz=1C5CHFA_enUS964US964&sxsrf=AOaemvIqoCYJW4GQFLEbgXUMP2753SPuLg:1630693916353&source=lnms&tbm=isch&sa=X&sqi=2&ved=2ahUKEwje9vucuOPyAhWZq5UCHXrcC14Q_AUoAnoECAEQBA&biw=1440&bih=788#imgrc=z28Ru8oWaBB90M"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Heros;
